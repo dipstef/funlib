f028f23199295850ff86a9135525dcb20007aaea
dipstef <dipstef@gmail.com>
2014-06-11 14:50:37 +0100

    updated tests

--- tests/decorator_test.py
+++ tests/decorator_test.py
@@ -12,8 +12,12 @@ def _test(add):
     def sum_plus5(x, y):
         return x + y
 
+    def sum_times2_plus7(x, y):
+        return sum_plus2(x, y) + sum_plus5(x, y)
+
     assert 5 == sum_plus2(1, 2)
     assert 8 == sum_plus5(1, 2)
+    assert 13 == sum_times2_plus7(1, 2)
 
     property_plus = property_decorator(add)
 
@@ -26,6 +30,9 @@ def _test(add):
         @add(value=5)
         def sum_plus5(self, x, y):
             return x + y
+
+        def sum_times2_plus7(self, x, y):
+            return self.sum_plus2(x, y) + self.sum_plus5(x, y)
 
         @property
         @add
@@ -57,16 +64,23 @@ def _test(add):
         def ten(self):
             return 7
 
+        @property
+        def twenty(self):
+            return self.nine + self.eleven
+
     sum = Sum()
 
     assert 5 == sum.sum_plus2(1, 2)
     assert 8 == sum.sum_plus5(1, 2)
+    assert 13 == sum.sum_times2_plus7(1, 2)
+
     assert 4 == sum.four
     assert 7 == sum.seven
     assert 9 == sum.nine
     assert 11 == sum.eleven
     assert 12 == sum.twelve
     assert 10 == sum.ten
+    assert 20 == sum.twenty
 
 
 def _test_decorator_fun():
@@ -82,7 +96,6 @@ def _test_decorator_fun():
 def _test_decorator_class():
     @decorator
     class add(object):
-
         def __init__(self, value=2):
             self._value = value
 
